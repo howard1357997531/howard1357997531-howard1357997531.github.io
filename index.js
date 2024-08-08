082678e139a75eaaaa447d0077fe844b639a30c9
howard1357997531 <howard1357997531@yahoo.com.tw>
2024-08-08 18:54:24 +0800

    asd

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ const fixButtomBtn = document.querySelector('.fix-buttom-btn')
 actualCaseSmbox1.forEach(el => el.addEventListener("mouseover", () => {
     const index = el.getAttribute('data-index')
     const img = document.querySelector(`.actual-case-img${index}`)
-    img.src = 'http://18.183.101.10/images/p2/case-back.jpg'
+    img.src = 'images/p2/case-back.jpg'
 }))
 
 actualCaseSmbox1.forEach(el => el.addEventListener("mouseleave", () => {
     const index = el.getAttribute('data-index')
     const img = document.querySelector(`.actual-case-img${index}`)
-    img.src = 'http://18.183.101.10/images/p2/case.jpg'
+    img.src = 'images/p2/case.jpg'
 }))
 
 actualCaseSmbox2.forEach(el => el.addEventListener("mouseover", () => {
     const index = el.getAttribute('data-index')
     const img = document.querySelector(`.actual-case-img${index}`)
-    img.src = `http://18.183.101.10/images/p2/Component-hover-${index}.jpg`
+    img.src = `images/p2/Component-hover-${index}.jpg`
 }))
 
 actualCaseSmbox2.forEach(el => el.addEventListener("mouseleave", () => {
     const index = el.getAttribute('data-index')
     const img = document.querySelector(`.actual-case-img${index}`)
-    img.src = `http://18.183.101.10/images/p2/company-${index}.jpg`
+    img.src = `images/p2/company-${index}.jpg`
 }))
 
 fixButtomBtn.addEventListener("mouseover", () => {
-    fixButtomBtn.src = "http://18.183.101.10/images/line-people02.png"
+    fixButtomBtn.src = "images/line-people02.png"
 })
 
 fixButtomBtn.addEventListener("mouseleave", () => {
-    fixButtomBtn.src = "http://18.183.101.10/images/line-people01.png"
+    fixButtomBtn.src = "images/line-people01.png"
 })
 
